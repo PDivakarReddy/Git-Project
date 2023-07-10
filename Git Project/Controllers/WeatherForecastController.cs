5c3399561e34efd92eaae3b5d3ff2d452c0f4cb7
DivakarReddyPeddaAsw <DivakarReddyPeddaAsw@VDL167>
2023-07-10 05:40:31 +0530

    Changes in Delete API

--- Git Project/Controllers/WeatherForecastController.cs
+++ Git Project/Controllers/WeatherForecastController.cs
@@ -35,7 +35,7 @@ namespace Git_Project.Controllers
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                Date = DateTime.Today.AddHours(index),
+                Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
